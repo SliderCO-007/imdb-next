61694967e930e7bf22185a6d14d218d508e2102f
Curtis Hamm <curt.hamm@gmail.com>
2024-11-23 21:15:07 -0600

    fetch data from IMDb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import Results from "@/components/Results";
 
-export default function Home() {
-  return (
-    <div>Home Page</div>
-  )
+const API_KEY = process.env.API_KEY
+
+
+// https://stackoverflow.com/questions/79143162/route-locale-used-params-locale-params-should-be-awaited-before-using
+export default async function Home({ searchParams }) {
+  // const genre = searchParams.genre || 'fetchTrending';
+  const { genre } = await searchParams || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  const results = data.results;
+  console.log(results);
+
+  return <div>
+    <Results results={results} />
+  </div>;
 }

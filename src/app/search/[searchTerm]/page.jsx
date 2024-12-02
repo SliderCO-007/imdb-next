0bc4db65ad79d52352d57af02366f608e750db78
Curtis Hamm <curt.hamm@gmail.com>
2024-12-01 19:31:25 -0600

    add Suspense for useSearchParams

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -5,7 +5,7 @@ export default async function SearchPage({ params }) {
     const searchTerm = ps.searchTerm
     const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=true`)
     const data = await res.json()
-    // console.log(`----------Searching for ${searchTerm}`);
+    console.log(`----------Searching for ${searchTerm}`);
     const results = data.results
   return <div>
         {
